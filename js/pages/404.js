9bc7d0df2507e464043eb5609ef7d63e9aa1e9d6
Aliph0th <theblacklord05@gmail.com>
2024-04-23 22:18:21 +0300

    fast fix 404

--- js/pages/404.js
+++ js/pages/404.js
@@ -2,7 +2,16 @@ import { pageElement } from '../variables/constants.js';
 import { createElement } from '../utils/helpers.js';
 
 export function NotFound() {
-   pageElement.appendChild(
-      createElement({ type: 'h1', classNames: ['notFound'], innerHTML: 'Page not found' })
+   pageElement.append(
+      createElement({
+         type: 'h1',
+         classNames: ['notFound'],
+         innerHTML: 'Page not found'
+      }),
+      createElement({
+         type: 'a',
+         attributes: { href: '#users' },
+         innerHTML: 'Go to home'
+      })
    );
 }
